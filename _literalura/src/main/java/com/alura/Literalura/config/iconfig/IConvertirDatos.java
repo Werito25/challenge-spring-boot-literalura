7d209d9bdcf14237a89f6438dcc9214cd66b1a37
Werito25 <168033775+Werito25@users.noreply.github.com>
2025-01-03 19:35:50 -0300

    Add files via upload

---
+++ _literalura/src/main/java/com/alura/Literalura/config/iconfig/IConvertirDatos.java
@@ -0,0 +1,8 @@
+package com.alura.Literalura.config.iconfig;
+
+public interface IConvertirDatos {
+
+    <T> T convertirDatosJsonAJava(String json , Class<T> clase); //metodo generico que coje el json y convierte en datos Java ya que no se sabe que retornara
+
+}
+
