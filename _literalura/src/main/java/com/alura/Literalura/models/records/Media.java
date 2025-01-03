7ed1928fc52a7d7367c9394620c722de6d0eaea7
Werito25 <168033775+Werito25@users.noreply.github.com>
2025-01-03 19:50:40 -0300

    Add files via upload

---
+++ _literalura/src/main/java/com/alura/Literalura/models/records/Media.java
@@ -0,0 +1,9 @@
+package com.alura.Literalura.models.records;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Media(
+        @JsonAlias("image/jpeg") String imagen
+){}
