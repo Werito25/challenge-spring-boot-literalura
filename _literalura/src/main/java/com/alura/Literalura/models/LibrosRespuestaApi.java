d7962ff83ff2d7c89da7d8c44f1f89286aeb1e17
Werito25 <168033775+Werito25@users.noreply.github.com>
2025-01-03 19:51:27 -0300

    Add files via upload

---
+++ _literalura/src/main/java/com/alura/Literalura/models/LibrosRespuestaApi.java
@@ -0,0 +1,22 @@
+package com.alura.Literalura.models;
+
+import com.alura.Literalura.models.records.DatosLibro;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LibrosRespuestaApi {
+
+    @JsonAlias("results")
+    List<DatosLibro> resultadoLibros;
+
+    public List<DatosLibro> getResultadoLibros() {
+        return resultadoLibros;
+    }
+
+    public void setResultadoLibros(List<DatosLibro> resultadoLibros) {
+        this.resultadoLibros = resultadoLibros;
+    }
+}
