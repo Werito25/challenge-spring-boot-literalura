9e02c4982d0364416a9cdcfdf50a1c95ff3bdd8d
Werito25 <168033775+Werito25@users.noreply.github.com>
2025-01-03 19:34:22 -0300

    Add files via upload

---
+++ _literalura/src/main/java/com/alura/Literalura/config/ConvertirDatos.java
@@ -0,0 +1,20 @@
+package com.alura.Literalura.config;
+
+import com.alura.Literalura.config.iconfig.IConvertirDatos;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConvertirDatos implements IConvertirDatos {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public <T> T convertirDatosJsonAJava(String json, Class<T> clase) {
+        try {
+            return objectMapper.readValue(json, clase);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
